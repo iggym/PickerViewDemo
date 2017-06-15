7d65e364c1ae4e6a2af6c49618cf6bd83eb5d1e2
iggym <iggy.mw@gmail.com>
2017-06-15 02:55:55 -0500

    picker view demo presenting a picker from the bottom

--- PickerViewDemo/FirstViewController.swift
+++ PickerViewDemo/FirstViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate , UITextFieldDelegate{
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var textView: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var buttonSelectionIndicator: UIButton!
+
+    @IBOutlet weak var textSelectionIndicator: UILabel!
+    
+    var pickOption = ["Dallas", "Waco", "Reno", "Plano", "Houston", "Austin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pickerView.delegate = self
+        self.textView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,39 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // returns the number of 'columns' to display.
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    @available(iOS 2.0, *)
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOption.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOption[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedOption = pickOption[row]
+        labelView.text = pickOption[row]
+        textView.text = "You selected \(selectedOption)"
+        
+    }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        labelView.text = "Cleared Selection"
+        textView.text = "Cleared Selection"
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+  
 }
 
--- PickerViewDemo/SecondViewController.swift
+++ PickerViewDemo/SecondViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate , UITextFieldDelegate{
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickOption = ["Dallas", "Waco", "Reno", "Plano", "Houston", "Austin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pickerView.isHidden = true
+        textField.text = pickOption[0]
+        textField.delegate = self
+        pickerView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // returns the number of 'columns' to display.
+    // returns the number of 'columns' to display.
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // returns the # of rows in each component..
+    @available(iOS 2.0, *)
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOption.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOption[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedOption = pickOption[row]
+        pickerView.isHidden = true;
+        textField.text = "You selected \(selectedOption)"
+        
+    }
 
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int)
+    {
+        textField.text = pickOption[row]
+        pickerView.isHidden = true;
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool   {
+        pickerView.isHidden = false
+        return false
+    }
 
 }
 
